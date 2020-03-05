167dfc7a8efb9572c817e4bd0f4e4a98612b0f99
Ionut Birlad <birladionut@yahoo.it>
2020-03-05 11:55:29 +0100

    aggiornato con invio messaggio

--- js/main.js
+++ js/main.js
@@ -1,5 +1,22 @@
 $(document).ready(function() {
 
+  var time = new Date();
+  var hours = time.getHours()
+  var min = time.getMinutes()
+  // var n = d.toLocaleTimeString();
+
+  $('.fa-paper-plane').click(function(){
+        var nomeInput = $('#msg').val();
+        $('#msg').val('');
+        // $('#nome-utente').html('<span class="rosso">' + nomeInput + '</span>');
+        // $('#nome-utente').text('<span class="rosso">' + nomeInput + '</span>');
+        // $('#nome-utente').text(nomeInput).attr('class', 'rosso').css('background-color', 'yellow');
+        // $('.lista-nomi').append('<div>' + nomeInput + '</div>');
+        var messaggio = $('.template .row').clone(); // Copia del contenuto del messaggio che è dentro al template (display none nel nostro CSS)
+        messaggio.find('.testo-messaggio').text(nomeInput); // Modifico il testo messaggio nel messaggio
+        messaggio.find('.orario').text(hours + ":" + min);
+        $('.main').append(messaggio); // Aggiungo in fondo alla lista nomi il messaggio
+    });
 
 
 
