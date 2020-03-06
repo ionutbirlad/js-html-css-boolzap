a3dcc1bbb63a070f3b4b02befae003d4ee883f18
Ionut Birlad <birladionut@yahoo.it>
2020-03-06 18:18:02 +0100

    pulizia codice

--- js/main.js
+++ js/main.js
@@ -1,39 +1,13 @@
 $(document).ready(function() {
 
   // Scroll down automatico
-  scroll(".main");
+    scroll(".main");
   // Fine scroll down automatico
 
-  //
-  var time = new Date();
-  var hours = time.getHours()
-  var min = time.getMinutes()
 
-
-
-  // Parte GESTIONE MESSAGGI
-  $(".msg .ir").click(function() {
-    textSend();
-  });
-  $("#msg").keypress(function(event) {
-    if (event.keyCode == 13) {
-      textSend();
-    }
-  });
-  // Fine parte GESTIONE MESSAGGI
-
-  // FOCUS/BLUR messaggi
-  $("#msg").focus(function() {
-    $(".ir").children("i").toggleClass("fas fa-microphone fas fa-paper-plane");
-  }).blur(function() {
-    $(".ir").children("i").toggleClass("fas fa-microphone fas fa-paper-plane");
-  });
-  // Fine FOCUS/BLUR messaggi
-
-// Ricerca CONTATTI
+  // Ricerca CONTATTI
     $(".search-txt").keyup(function(event) {
       var carattereInserito = $(this).val().toLowerCase();
-      // console.log(carattereInserito);
       $(".contatti .item .nm h6").each(function() {
         if ($(this).text().toLowerCase().includes(carattereInserito)) {
           $(this).parents(".contatti .item").show("fast");
@@ -42,33 +16,10 @@ $(document).ready(function() {
         }
       });
     });
-// Fine ricerca CONTATTI
+  // Fine ricerca CONTATTI
 
 
-
-
-
-
-    // emoticons
-    $(".far.fa-smile").click(function() {
-      if ($(".emtc .emoticons").is(":visible")) {
-        $(".emtc .emoticons").toggle();
-      } else {
-        $(".is .cerca").hide();
-        $(".emtc .emoticons").toggle();
-      }
-    });
-
-    // ricerca chat
-    $(".fas.fa-search").click(function() {
-      if ($(".is .cerca").is(":visible")) {
-        $(".is .cerca").toggle();
-      } else {
-        $(".emtc .emoticons").hide();
-        $(".is .cerca").toggle();
-      }
-    });
-
+  // Ricerca MESSAGGI
     $(".cerca input").keyup(function(event) {
       var carattereImmesso = $(this).val().toLowerCase();
       // console.log(carattereInserito);
@@ -80,61 +31,99 @@ $(document).ready(function() {
         }
       });
     });
+  // Fine ricerca MESSAGGI
+
+
+  // Parte GESTIONE MESSAGGI
+    $(".msg .ir").click(function() {
+      textSend();
+    });
+    $("#msg").keypress(function(event) {
+      if (event.keyCode == 13) {
+        textSend();
+      }
+    });
+  // Fine parte GESTIONE MESSAGGI
+
+  // FOCUS/BLUR messaggi
+    $("#msg").focus(function() {
+      $(".ir").children("i").toggleClass("fas fa-microphone fas fa-paper-plane");
+    }).blur(function() {
+      $(".ir").children("i").toggleClass("fas fa-microphone fas fa-paper-plane");
+    });
+  // Fine FOCUS/BLUR messaggi
+
+
+  // Finestra EMOTICONS
+    $(".far.fa-smile").click(function() {
+      if ($(".emtc .emoticons").is(":visible")) {
+        $(".emtc .emoticons").toggle();
+      } else {
+        $(".is .cerca").hide();
+        $(".emtc .emoticons").toggle();
+      }
+    });
+  // Fine finestra EMOTICONS
+
+  // Finestra RICERCA CHAT
+    $(".fas.fa-search").click(function() {
+      if ($(".is .cerca").is(":visible")) {
+        $(".is .cerca").toggle();
+      } else {
+        $(".emtc .emoticons").hide();
+        $(".is .cerca").toggle();
+      }
+    });
+  // Fine finestra RICERCA CHAT
 
 
 
 
+  // Parte delle FUNZIONI
 
-// Parte delle FUNZIONI
+  // Funzione per l'invio dei messaggi
+    function textSend() {
+      var messaggioInserito = $("#msg").val();
+      console.log(messaggioInserito);
+      if (messaggioInserito.trim().length > 0) {
+        $("#msg").val("");
+        messageGenerator(messaggioInserito, "sent");
+        scroll(".main");
+        setTimeout(function() {
+          messageGenerator("ok", "received");
+          scroll(".main");
+        }, 1000);
+      }
+    }
 
-// Funzione per l'invio dei messaggi
-function textSend() {
-  var messaggioInserito = $("#msg").val();
-  console.log(messaggioInserito);
-  if (messaggioInserito.trim().length > 0) {
-    $("#msg").val("");
-    messageGenerator(messaggioInserito, "sent");
-    scroll(".main");
-    setTimeout(function() {
-      messageGenerator("ok", "received");
-      scroll(".main");
-    }, 1000);
-  }
-}
+  // Funzione per creare il messaggio
+    function messageGenerator(text, sentReceived) {
+      var templateClone = $(".template .row").clone();
+      templateClone.find(".message-text").text(text);
+      templateClone.find(".message-time").text(new Date().getHours() + ":" + new Date().getMinutes());
+      templateClone.children(".message").addClass(sentReceived);
+      $(".main").append(templateClone);
+    }
 
-// Funzione per creare il messaggio
-function messageGenerator(text, sentReceived) {
-  var templateClone = $(".template .row").clone();
-  templateClone.find(".message-text").text(text);
-  templateClone.find(".message-time").text(new Date().getHours() + ":" + new Date().getMinutes());
-  templateClone.children(".message").addClass(sentReceived);
-  $(".main").append(templateClone);
-}
+  // Funzione generica per lo scroll totale
 
-// Funzione generica per lo scroll totale
+  // Alternativa di Luca ==> non funziona perfettamente
 
-// Alternativa di Luca
+    /*
+    function scroll(yourDiv) {
+      var pixelScroll = $(yourDiv).height();
+      $(yourDiv).scrollTop(pixelScroll);
+    }
+    */
 
-/*
-function scroll(yourDiv) {
-  var pixelScroll = $(yourDiv).height();
-  $(yourDiv).scrollTop(pixelScroll);
-}
-*/
+  // Alternativa con animazione
+    function scroll(yourDiv) {
+      $(yourDiv).animate({scrollTop:
+       $(yourDiv).get(0).scrollHeight
+      }, 1000);
+    }
 
-// Alternativa con animazione
-function scroll(yourDiv) {
-  $(yourDiv).animate({scrollTop:
-   $(yourDiv).get(0).scrollHeight
-  }, 1000);
-}
-
-
-
-
-
-
-// Fine parte delle FUNZIONI
+  // Fine parte delle FUNZIONI
 
 
 
